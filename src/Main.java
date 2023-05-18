8562c42fff9075f4d979025c0e442890d79b03ce
WellingtonZs <wellingtonzsescola@gmail.com>
2023-05-18 08:21:05 -0300

    orientação de objetos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-+public class Main {
+public class Main {
     public static void main(String[] args) {
-
+        //aula de orientação de objetos
+        //declarar objetos
+        Pessoa adao;
+        //instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
     }
 }
