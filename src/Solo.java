adebf3348b5a543ce761e8b4f78c410adf9b6fb1
Wellington Cavalcante Ferreira de Lima <wellingtonzsescola@gmail.com>
2023-06-13 08:12:03 -0300

    sobrescrita do método

---
+++ src/Solo.java
@@ -0,0 +1,13 @@
+public class Solo {
+    String tipo;
+    String cor;
+    String substancias;
+    boolean fertil;
+
+    public Planta gerarPlant(
+        Semente s1
+    ){
+        Planta p1 = new Planta();
+        return p1;
+    }
+}
